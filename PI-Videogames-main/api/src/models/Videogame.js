194fa739b5fd55e053074f972ee80d7bbac097db
macarena <singhmacarena.r@gmail.com>
2022-05-23 16:48:57 -0300

    videogames db table created

--- PI-Videogames-main/api/src/models/Videogame.js
+++ PI-Videogames-main/api/src/models/Videogame.js
@@ -4,9 +4,36 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('videogame', {
+    id:{
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    launch_date: {
+      type: DataTypes.STRING,
+    },
+    rating: {
+      type: DataTypes.FLOAT
+    },
+    image: {
+      type: DataTypes.STRING
+    },
+    platforms: {
+      type: DataTypes.ARRAY(DataTypes.STRING),
+      allowNull: false,
+    },
+    vg_created_db: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: true
+    }
   });
 };
