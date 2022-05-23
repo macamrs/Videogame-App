0722859f5c211b5b8ffcaab37c4005d093acabed
macarena <singhmacarena.r@gmail.com>
2022-05-23 17:31:21 -0300

    association of tables genre and videogames

--- PI-Videogames-main/api/src/routes/index.js
+++ PI-Videogames-main/api/src/routes/index.js
@@ -1,12 +1,17 @@
 const { Router } = require('express');
+const vgRouter = require('./vgRouter');
+const genreRouter = require('./genreRouter')
+
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 // Hacer callbacks de los errores
+
+router.use("/videogames", vgRouter)
+router.use("/genres", genreRouter)
 
 module.exports = router;
