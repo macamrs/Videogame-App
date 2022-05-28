34a5aec4152c683b735cb43e78a491d454ca23b2
macarena <singhmacarena.r@gmail.com>
2022-05-28 10:38:58 -0300

    routes update

---
+++ PI-Videogames-main/api/src/routes/gameRouter.js
@@ -0,0 +1,15 @@
+const { Router } = require('express');
+const router = Router();
+const { createVideoGame, gameDetail, gameDeleted } = require('../control/gameControl')
+
+
+// POST /videogame (create a videogame)
+router.post('/', createVideoGame)
+
+// GET /videogame/{idVideogame}
+router.get('/:id', gameDetail)
+
+// DELETE /videogame/{idVideogame}
+router.delete('/:id', gameDeleted)
+
+module.exports = router;
