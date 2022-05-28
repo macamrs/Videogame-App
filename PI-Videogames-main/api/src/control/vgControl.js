bfa44b69dd1e100418eae6d178b5bacfab2c7588
macarena <singhmacarena.r@gmail.com>
2022-05-28 10:39:48 -0300

    function logic to GET all viodegames and GET videogame by query req

--- PI-Videogames-main/api/src/control/vgControl.js
+++ PI-Videogames-main/api/src/control/vgControl.js
@@ -1,13 +1,72 @@
-const axios = require('axios')
+// &page_size=100 para limitar juegos tomados 
+// GET all games(from data base also)
+// http://localhost:3001/videogames
+// http://localhost:3001/videogames/red-dead-redemption-undead-nightmare
 
-function getAllVideoGames (req, res, next) {
+const axios = require("axios");
+require('dotenv').config();
+const { API_KEY } = process.env;
+const { Videogame, Genre } = require("../db");
+  
+async function gameAPI () {
     try {
-        let videogames = (await axios('https://api.rawg.io/api/games')).data.results
-        res.send(videogames)
+      let arrGames = [];
+      for (let i = 1; i < 6; i++) {
+        const gamesByPage = await axios(`https://api.rawg.io/api/games?key=${API_KEY}&page=${i}`);
+        const gameResults = gamesByPage.data.results;
+        arrGames = arrGames.concat(gameResults);
+      }
+  
+      const gamesApi = arrGames.map( g => {  
+        return {
+          id: g.id,
+          name: g.name,
+          image: g.background_image,
+          rating: g.rating,
+          genres: g.genres.map(g => g.name),
+          launch_date: g.released,
+          platforms: g.platforms.map(p => p.platform.name)
+        };
+      });
+  
+      return gamesApi
+
     } catch (error) {
-        next(error)
+      console.log(error); 
     }
-}
+  };
+  
+async function gamesDB () {
+    const gameDB = await Videogame.findAll({
+        include: {
+        model: Genre,
+        attributes: ["name"]
+        }
+    })
+    return gameDB;
+};
+  
+async function getAllVideoGames (req, res) {
+    const { name } = req.query;
+    const infoDB = await gamesDB();
+    const infoAPI = await gameAPI();
+    const allGames = infoAPI.concat(infoDB) // [...dataDb, ...dataApi];
+
+    if(name) {
+      let gameByName = allGames.filter(g => g.name.toLowerCase().includes(name.toLowerCase()));
+      
+      if (!gameByName.length) {
+        return res.send('Game not in Data Base')
+      } else if(gameByName.length > 15) {
+        return gameByName = gameByName.slice(0, 15)
+      } else {
+        return res.send(gameByName)
+      }
+
+    }
+
+    return res.status(200).send(allGames);
+  };
 
 module.exports = {
     getAllVideoGames
