7a8106b92169c3a8b374c933f3c54b41feedc6af
macarena <singhmacarena.r@gmail.com>
2022-06-07 17:22:06 -0300

    added parseInt for vg in data base

--- PI-Videogames-main/client/src/components/GameCard/Game.js
+++ PI-Videogames-main/client/src/components/GameCard/Game.js
@@ -3,11 +3,11 @@ import './Game.css'
 import { Link } from 'react-router-dom'
 import star from '../../images/icons8-rating-32.png'
 
-export default function Game({id, name, image, genres, rating}) {
+export default function Game({id, name, image, genres, rating, vg_created_db}) {
 
     return (
         <div className='card_game'>
-            <Link className='link' to={`/details/${id}`} key={id} >
+            <Link className='link' to={`/details/${id}`} key={parseInt(id)} >
                 <h2 className='title'>{name}</h2>
                 <img src={image} width='300px' alt='#'/>
                     <div className='info'>
