e11b3e16ada4cd92614713e421a1338a24c96da9
macarena <singhmacarena.r@gmail.com>
2022-06-08 14:05:13 -0300

    prev and next btn

--- PI-Videogames-main/client/src/components/Pagination/Pagination.js
+++ PI-Videogames-main/client/src/components/Pagination/Pagination.js
@@ -1,7 +1,9 @@
 import React from 'react'
+import { useState } from 'react'
 import './Pagination.css'
 
 export default function Pagination({gamesPerPage, totalGames, paginate}) {
+    const [actual, setActual] = useState(1)
     const pageNumbers = []
 
     // if index is less or equal to the total games divided by the gamesperoPage
@@ -9,14 +11,40 @@ export default function Pagination({gamesPerPage, totalGames, paginate}) {
         pageNumbers.push(i)
     }
 
+    function handlePrev(e) {
+        e.preventDefault();
+        if(actual === 1) {
+            paginate(1)
+        } else {
+            paginate(actual - 1)
+            setActual(actual - 1)
+        }
+    }
+
+    function handleNext(e) {
+        e.preventDefault();
+        if(actual === pageNumbers.length) {
+            paginate(pageNumbers.length)
+        } else {
+            paginate(actual + 1)
+            setActual(actual + 1)
+        }
+    }
+
     return (     
         <div>
             <div className='page_container'>
+                <div>
+                    <button className='btn' onClick={e => handlePrev(e)}>Previous</button>
+                </div>
             {pageNumbers?.map(number => {
                 return (<li key={number}>
                     <button className='btn_paginate' value={number} onClick={() => paginate(number)}>{number}</button>
                 </li>)
             })}
+                <div>
+                    <button className='btn' onClick={e => handleNext(e)}>Next</button>
+                </div>
             </div>
         </div>
     )
